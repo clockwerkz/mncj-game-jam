26b92b4d9bb0c4a7a4f7f58952dfe0c922c2d0d2
Jared Wasserman <jaredbwasserman@gmail.com>
2021-03-27 04:01:47 -0400

    Add transformation logic with delays

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,23 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: How do you keep the momentum for in/out moon beams?
+// TODO: Should transforming be instant or have a time component?
+// TODO: Should inputs be locked during pending transform and/or transforming?
+// TODO: Seprate horizontal speed for pirate/bird?
+// TODO: Transition(s) animations
+// TODO: Bird cannot interact with physical objects (platforms and barrels etc.)
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Settings to Tweak for Gameplay Feel")]
-    public float moveSpeed = 3;
-    public float jumpForce = 20;
-    public float normalGravity = 5f;
-    public float flightGravity = 3f;
+    public float moveSpeed;
+    public float jumpForce;
+    public float normalGravity;
+    public float flightGravity;
+    public float intoMoonDelaySeconds;
+    public float outOfMoonDelaySeconds;
 
     [Space(10.0f)]
     [Header("Ground Detection Settings")]
     public Transform feet;
     public LayerMask groundLayer;
 
+    // Components
     private Rigidbody2D _rigidBody2d;
     private SpriteRenderer _spriteRenderer;
+
+    // Horizontal movement input
     private float _mx;
+
+    // Keep track of whether the player is inside a moonbeam
     private bool _inMoonLight = false;
+
+    // Counters for going in/out of moonbeams
+    private float _intoMoonCounter;
+    private float _outOfMoonCounter;
+
+    // Keep track of Player transformations
+    private enum PlayerTransform
+    {
+        Pirate,
+        Bird
+    }
+    private PlayerTransform _curTransform = PlayerTransform.Pirate;
+    private PlayerTransform _prevTransform = PlayerTransform.Pirate;
 
     // Start is called before the first frame update
     void Start()
@@ -36,13 +63,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update moonbeam counters
+        if (_intoMoonCounter > 0)
+        {
+            _intoMoonCounter -= Time.deltaTime;
+        }
+        if (_outOfMoonCounter > 0)
+        {
+            _outOfMoonCounter -= Time.deltaTime;
+        }
+
+        // Maybe transform
+        MaybeTransform();
+
+        // Calculate horizontal velocity
         Vector2 movement = new Vector2(_mx * moveSpeed, _rigidBody2d.velocity.y);
         _rigidBody2d.velocity = movement;
 
-        if (Input.GetButtonDown("Jump") &&  _inMoonLight)
+        // Calculate jump
+        if (Input.GetButtonDown("Jump") && _curTransform == PlayerTransform.Bird)
         {
             Jump();
         }
+
+        // Update previous transform after everything else
+        _prevTransform = _curTransform;
     }
 
     bool IsGrounded()
@@ -57,13 +102,31 @@ public class PlayerMovement : MonoBehaviour
         _rigidBody2d.velocity = movement;
     }
 
+    void MaybeTransform()
+    {
+        // Stayed in moonbeam long enough to become a bird
+        if (_intoMoonCounter < 0 && _prevTransform == PlayerTransform.Pirate && _inMoonLight)
+        {
+            _curTransform = PlayerTransform.Bird;
+            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 0.5f);
+            _rigidBody2d.gravityScale = flightGravity;
+        }
+
+        // Stayed out of moonbeam long enough to become a pirate
+        if (_outOfMoonCounter < 0 && _prevTransform == PlayerTransform.Bird && !_inMoonLight)
+        {
+            _curTransform = PlayerTransform.Pirate;
+            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 1.0f);
+            _rigidBody2d.gravityScale = normalGravity;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "MoonBeam")
         {
             _inMoonLight = true;
-            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 0.5f);
-            _rigidBody2d.gravityScale = flightGravity;
+            _intoMoonCounter = intoMoonDelaySeconds;
         }
     }
 
@@ -72,8 +135,7 @@ public class PlayerMovement : MonoBehaviour
         if (col.gameObject.tag == "MoonBeam")
         {
             _inMoonLight = false;
-            _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 1.0f);
-            _rigidBody2d.gravityScale = normalGravity;
+            _outOfMoonCounter = outOfMoonDelaySeconds;
         }
     }
 }
